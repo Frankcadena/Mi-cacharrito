1f27a68c7f5026bece6a2168280240241e1a1868
USUARIO <USUARIO@DESKTOP-EGCJVT7>
2024-02-25 19:36:08 -0500

    Casos de uso usuario

--- cacharrito/src/main/java/com/example/demo/controlador/UsuarioControlador.java
+++ cacharrito/src/main/java/com/example/demo/controlador/UsuarioControlador.java
@@ -1,7 +1,27 @@
 package com.example.demo.controlador;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.modelo.Usuario;
+import com.example.demo.repositorio.RepositorioUsuario;
+
+@RestController
+@RequestMapping("/ver/e1/")
 
 public class UsuarioControlador {
+
+	@Autowired
+	private RepositorioUsuario repositorio;
+	
+	
+	@GetMapping("/consultar")
+	public List<Usuario>verTodosUsuarios(){
+		return repositorio.findAll();
+	}
 	
 }
--- cacharrito/src/main/java/com/example/demo/modelo/Carro.java
+++ cacharrito/src/main/java/com/example/demo/modelo/Carro.java
@@ -1,6 +1,61 @@
 package com.example.demo.modelo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Carros")
 
 public class Carro {
-       
+
+        @Id
+        @Column(name="numero_placa")
+        private Long placa;
+
+            @Column(name = "cantidad_puesto", length = 100, nullable = false )
+            private Integer cantidadPuesto;
+
+            @Column(name = "disponibilidad", length = 100, nullable = false )
+            private String disponibilidad;
+
+			public Carro() {
+				super();
+				// TODO Auto-generated constructor stub
+			}
+
+			public Carro(Long placa, Integer cantidadPuesto, String disponibilidad) {
+				super();
+				this.placa = placa;
+				this.cantidadPuesto = cantidadPuesto;
+				this.disponibilidad = disponibilidad;
+			}
+
+			public Long getPlaca() {
+				return placa;
+			}
+
+			public void setPlaca(Long placa) {
+				this.placa = placa;
+			}
+
+			public Integer getCantidadPuesto() {
+				return cantidadPuesto;
+			}
+
+			public void setCantidadPuesto(Integer cantidadPuesto) {
+				this.cantidadPuesto = cantidadPuesto;
+			}
+
+			public String getDisponibilidad() {
+				return disponibilidad;
+			}
+
+			public void setDisponibilidad(String disponibilidad) {
+				this.disponibilidad = disponibilidad;
+			}
+            
+            
 }
+
